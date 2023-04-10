d16750449c0109f61aad1b932f3679d654e709fb
m.kamwana <maxwell.kamwana@gmail.com>
2023-04-10 19:31:28 +0100

    Updating the Climate starter code

---
+++ app.py
@@ -0,0 +1,46 @@
+# Import the dependencies.
+import numpy as np
+
+import sqlalchemy
+import datetime as dt
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify 
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save references to each table
+Measurement= Base.classes.measurement
+Station= Base.classes.station
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/names<br/>"
+        f"/api/v1.0/passengers"
+    )
+
+#################################################
+# Flask Routes
+#################################################
